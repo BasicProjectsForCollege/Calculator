7341286635993b091fcf1030120bbcdce402fb89
SarthakMP <106466539+SarthakMP@users.noreply.github.com>
2025-01-23 19:04:16 +0530

    Update1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Shapes;
 using System.Diagnostics;
 using System.Security.Cryptography.X509Certificates;
 using System.Text.Json;
+using System.Windows.Media.TextFormatting;
+using System.Numerics;
 
 namespace WpfApp1
 {
@@ -31,7 +33,70 @@ namespace WpfApp1
 
         private void Print_digits(object sender, RoutedEventArgs e)
         {
-            string tag = "\0";
+            string tag = "1\0";
+            if(sender is Button button)
+            {
+                tag = button.Tag?.ToString();
+            }
+            int a = Int32.Parse(tag);
+            switch (a) {
+                case 0:
+                    {
+                        textbox1.Text += num0.Content.ToString();
+                        break;
+                    }
+                case 1:
+                    {
+                        textbox1.Text += num1.Content.ToString();
+                        break;
+                    }
+                case 2:
+                    {
+                        textbox1.Text += num2.Content.ToString();
+                        break;
+                    }
+                case 3:
+                    {
+                        textbox1.Text += num3.Content.ToString();
+                        break;
+                    }
+                case 4:
+                    {
+                        textbox1.Text += num4.Content.ToString();
+                        break;
+                    }
+                case 5:
+                    {
+                        textbox1.Text += num5.Content.ToString();
+                        break;
+                    }
+                case 6:
+                    {
+                        textbox1.Text += num6.Content.ToString();
+                        break;
+                    }
+                case 7:
+                    {
+                        textbox1.Text += num7.Content.ToString();
+                        break;
+                    }
+                case 8:
+                    {
+                        textbox1.Text += num8.Content.ToString();
+                        break;
+                    }
+                case 9:
+                    {
+                        textbox1.Text += num9.Content.ToString();
+                        break;
+                    }
+                case 10:
+                    {
+                        break;
+                    }
+
+            }
+
 
             return;
 
@@ -39,7 +104,8 @@ namespace WpfApp1
         private void Print_Arthermatic_op(object sender, RoutedEventArgs e)
         {
             string tag = "1\0";
-            if(sender is Button button)
+            string text = textbox1.Text;
+            if (sender is Button button)
             {   
                 tag = button.Tag?.ToString();
             }
@@ -47,7 +113,7 @@ namespace WpfApp1
             switch (a) {
             case 101:
             {
-                        textbox1.Text += equal.Content.ToString();
+                        evaluate(text);
                 break;
             }
             case 102:
@@ -70,19 +136,48 @@ namespace WpfApp1
                         textbox1.Text += div.Content.ToString();
                         break;
             }
-
-            default:
+            case 106:
             {
-                       return;
+                        textbox1.Text += per.Content.ToString();
+                        break;
             }
-        }
+            case 107:
+            {
+                        break;
+            }
+            case 108:
+            {
+
+                        textbox1.Text = "";
+                        break;
+            }
+        
+            
+            }
 
         }
         
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void evaluate(String a)
         {
-             
+            int size = a.Length,c=0;
+            List<string> val = new List<string>();
+            List<char> op = new List<char>();
+            List<char> val1 = new List<char>();
+            for (int i =0; i<size; i++)
+            {
+                if (a[i]<47 && a[i] >58)
+                {
+                    
+                    op.Add(a[i]);
+                }
+                else
+                {
+                    val1.Add(a[i]);
+                }
+            }
+            MessageBox.Show(val[0]);
         }
+
+        
     }
 }
