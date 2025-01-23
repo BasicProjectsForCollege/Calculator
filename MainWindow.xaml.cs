179901dade558988b5b5491e9d9d924e3ed4052f
SarthakMP <106466539+SarthakMP@users.noreply.github.com>
2025-01-23 15:12:52 +0530

    changes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -20,6 +21,7 @@ namespace WpfApp1
     public partial class MainWindow : Window
     {
         string ans="\0";
+        int val;
         public MainWindow()
         {
             
@@ -27,104 +29,60 @@ namespace WpfApp1
             
         }
 
+        private void Print_digits(object sender, RoutedEventArgs e)
+        {
+            string tag = "\0";
+
+            return;
+
+        }
+        private void Print_Arthermatic_op(object sender, RoutedEventArgs e)
+        {
+            string tag = "1\0";
+            if(sender is Button button)
+            {   
+                tag = button.Tag?.ToString();
+            }
+            int a = Int32.Parse(tag);
+            switch (a) {
+            case 101:
+            {
+                        textbox1.Text += equal.Content.ToString();
+                break;
+            }
+            case 102:
+            {
+                        textbox1.Text += sub.Content.ToString();
+                        break;
+            }
+            case 103:
+            {
+                        textbox1.Text += add.Content.ToString();
+                        break;
+            }
+            case 104:
+            {
+                        textbox1.Text += mul.Content.ToString();
+                        break;
+            }
+            case 105:
+            {
+                        textbox1.Text += div.Content.ToString();
+                        break;
+            }
+
+            default:
+            {
+                       return;
+            }
+        }
+
+        }
         
-        private void Print_1(object sender, RoutedEventArgs e)
-        {
-            //update text
-            textbox1.TextChanged -= TextBox_TextChanged;
-
-            //change of text
-            string s = JsonSerializer.Serialize(button1.Content);
-            string tmp1 = s.Trim('"');
-            textbox1.Text += tmp1;
-
-            //changed string
-            ans += textbox1.Text;
-            return;
-
-        }
-        private void Print_plus(object sender, RoutedEventArgs e)
-        {
-            //update text
-            textbox1.TextChanged -= TextBox_TextChanged;
-
-            //change of text
-            string s = JsonSerializer.Serialize(add.Content);
-            string tmp1 = s.Trim('"');
-            textbox1.Text += tmp1;
-
-            //changed string
-            ans += textbox1.Text;
-            return;
-
-        }
-        private void Print_minus(object sender, RoutedEventArgs e)
-        {
-            //update text
-            textbox1.TextChanged -= TextBox_TextChanged;
-
-            //change of text
-            string s = JsonSerializer.Serialize(sub.Content);
-            string tmp1 = s.Trim('"');
-            textbox1.Text += tmp1;
-
-            //changed string
-            ans += textbox1.Text;
-            return;
-
-        }
-        private void Print_star(object sender, RoutedEventArgs e)
-        {
-            //update text
-            textbox1.TextChanged -= TextBox_TextChanged;
-
-            //change of text
-            string s = JsonSerializer.Serialize(mul.Content);
-            string tmp1 = s.Trim('"');
-            textbox1.Text += tmp1;
-
-            //changed string
-            ans += textbox1.Text;
-            return;
-
-        }
-
-        private void Print_slash(object sender, RoutedEventArgs e)
-        {
-            //update text
-            textbox1.TextChanged -= TextBox_TextChanged;
-
-            //change of text
-            string s = JsonSerializer.Serialize(div.Content);
-            string tmp1 = s.Trim('"');
-            textbox1.Text += tmp1;
-
-            //changed string
-            ans += textbox1.Text;
-            return;
-
-        }
-
-        private void Print_equal(object sender, RoutedEventArgs e)
-        {
-            //update text
-            textbox1.TextChanged -= TextBox_TextChanged;
-
-            //change of text
-            string s = JsonSerializer.Serialize(equal.Content);
-            string tmp1 = s.Trim('"');
-            textbox1.Text += tmp1;
-
-            //changed string
-            ans += textbox1.Text;
-            return;
-
-        }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            
-            button1.Click += Print_1; 
+             
         }
     }
 }
