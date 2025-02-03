18f8dd620b42cd345a8f08b4ae3fde175d1f11a9
SarthakMP <106466539+SarthakMP@users.noreply.github.com>
2025-02-03 21:36:50 +0530

    update

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,21 +1,8 @@
 ﻿
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Diagnostics;
-using System.Security.Cryptography.X509Certificates;
-using System.Text.Json;
-using System.Windows.Media.TextFormatting;
-using System.Numerics;
-using System.Drawing;
-using System.Security.Policy;
+
+
 
 namespace WpfApp1
 {
@@ -24,15 +11,18 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
-        
-        int val;
+
+        double pi = 3.141592;
+        double e = 2.7182818;
+         Stack<double> val = new Stack<double>();
+         Stack<string> op = new Stack<string>();
         public MainWindow()
         {
-
             InitializeComponent();
-
         }
+        public double ans = 0;
 
+        public int multi = 10;
         private void Print_digits(object sender, RoutedEventArgs e)
         {
             string tag = "1\0";
@@ -121,12 +111,12 @@ namespace WpfApp1
                 tag = button.Tag?.ToString();
             }
             int a = Int32.Parse(tag);
-            float ans;
+            double ans;
             switch (a) {
                 case 101:
                     {
-                        
-                        ans =evaluate(text);
+
+                        ans = evaluate(text);
                         textbox1.Text = ans.ToString();
                         break;
                     }
@@ -152,6 +142,7 @@ namespace WpfApp1
                     }
                 case 106:
                     {
+                        
                         textbox1.Text += per.Content.ToString();
                         break;
                     }
@@ -165,24 +156,16 @@ namespace WpfApp1
                         textbox1.Text = "";
                         break;
                     }
+                case 109:
+                    {
+                        textbox1.Text += "!";
+                        break;
+                    }
 
 
             }
 
         }
-
-        private char check_op(char c)
-        {
-            switch (c) {
-                case '+': { return '+';}
-                case '-': { return '-'; }
-                case '*': { return '*'; }
-                case '/': { return '/'; }
-            }
-            return '_';
-
-        }
-
         private void Print_Trig(object sender, RoutedEventArgs e)
         {
             string tag = "";
@@ -202,104 +185,242 @@ namespace WpfApp1
 
 
         }
+        private double trignometery(double a, string op)
+        {
 
+  
+            switch (op) {
+
+                case "sin":
+                    {
+                        a = a * pi * 0.005555;
+                        double res = (a - (expontential(a, 3) * (0.1666667)) + (expontential(a, 5) * (0.0083333)) - (expontential(a, 7) * 0.0001984));
+                        return res;
+
+                    }
+                case "cos":
+                    {
+                        a = a * pi * 0.005555;
+                        double res = (1 - (expontential(a, 2) * 0.5) + (expontential(a, 4) * 0.0416667) - (expontential(a, 6) * 0.0013889));
+                        return res;
+
+                    }
+                case "tan":
+                    {
+                        a = a * pi * 0.005555;
+                        double res = (a + expontential(a, 3) * 0.3333334 + expontential(a, 5) * 0.1333334);
+                        return res;
+                    }
+
+            
+        }
+
+            return 0;
+
+        }
+        private double logarithmic(double x, string op)// x = y*2^k
+        {
+            double K,y,z,z2,power;
+            ans = 0;
+            int i = 1;
+            switch (op) {
+                case "ln":
+                    {
+                        //funtion to calculate ln
+                        K = 0;
+                        y = x;
+                        while (y > 2)
+                        {
+                            y /= 2;
+                            K += 0.6931471;
+                        }
+                        while (y < 1)
+                        {
+                            y *= 2;
+                            K -= 0.6931471;
+                        }
+
+                        z = (y - 1) / (y + 1);
+                        z2 = z * z;
+                        power = z;
+                        i = 1;
+                        while (i < 2 * 20)
+                        {
+                            ans += power / i;
+                            power *= z2;
+                            i += 2;
+                        }
+                        return 2 * ans + K;
+
+                    }
+                case "log":
+                    {
+                        K = 0;
+                        y = x;
+                        while (y > 2)
+                        {
+                            y /= 2;
+                            K += 0.6931471;
+                        }
+                        while (y < 1)
+                        {
+                            y *= 2;
+                            K -= 0.6931471;
+                        }
+                        
+                        z = (y - 1) / (y + 1);
+                        
+                        z2 = z * z;
+                        ans = 0;
+                        power = z;
+                        i = 1;
+                        while (i < 2 * 20)
+                        {
+                            ans += power / i;
+                            power *= z2;
+                            i += 2;
+                        }
+                        return (2 * ans + K) * 0.4343;
+                    }
+        }
+            return 0;
+        }
+        private double expontential(double x, double y) //x^y
+        { //add a flag
+            double ans = 1;
+
+            if(x == e)
+            {
+                while (y > 0)
+                {
+                    ans *= e;
+                    y--;
+                }
+
+            }
+            else
+            {
+                while (y > 0)
+                {
+                    ans *= x;
+                    y--;
+                }
+            }
+
+            return ans;
+        }
+        private double factorial(double x)
+        {
+            ans=1;
+            while (x > 0)
+            {
+                ans = x*ans;
+                x--; 
+            }
+            return ans;
+        }
+        private void check_op()
+        {
+            if (op.Peek() == "sin" || op.Peek() == "cos" || op.Peek() == "tan")
+            {
+                val.Push(trignometery(val.Pop(), op.Pop()));
+            }
+            else if (op.Peek() == "ln" || op.Peek() =="log")
+            {
+                val.Push(logarithmic(val.Pop(), op.Pop()));
+            }
+            else if (op.Peek() == "!")
+            {
+                val.Push(factorial(val.Pop()));
+                
+                op.Pop();
+            }else if(op.Peek() == "^")
+            {//error for e^(x!)
+                double y = val.Pop();
+                
+                double x = val.Pop();
+                val.Push(expontential(x, y));
+                op.Pop();
+            }
+        }
         //arthematic funtion
-        private float arthematic(float prev,float next, char op)
+        private double arthematic(double prev,double next, string op)
         {
             
             switch (op) {
-                case '+': {
+                case "+": {
                         return prev + next;
                     }
-                case '-':
+                case "-":
                     {
                         return prev - next;
                     }
-                case '*':
+                case "*":
                     {
                         return prev * next;
                     }
-                case '/':
+                case "/":
                     {
                         return prev / next;
                     }
+                
 
             }
             return 0;
 
         }
-        private int Precedence1(char op)
+        private int Precedence1(string op)
         {
-            if (op == '+' || op == '-') return 1;
-            if (op == '*' || op == '/') return 2;
+            if (op == "+" || op == "-") return 1;
+            if (op == "*" || op == "/") return 2;
+            if(op == "sin" || op=="cos"||op=="tan"||op =="log" ||op=="!")  return 3;
+            if (op == "^") return 4;
             return 0;
         }
-        private string add_brac(String a)
-        {
-            /*int size = a.Count();            
-            a = a.Insert(0, "(");
-            a = a.Insert(size + 1, ")");
-            int i = 0;
-            int c = 0;
-            size = a.Length;
-            for (i = 0; i < size; i++)
-            {
-                if (a[i + c] == '+' || a[i + c] == '-' )
-                {
-                    a = a.Insert(i + c + 1, "(");
-                    a = a.Insert(i + c, ")");
-                    c = c + 2;
-                }
-            }*/
-            return  "("+a+")";
-            
-        }
-        
-
-
-        
         //evalution 5th attempt
-        private float evaluate(String a)
+        private double evaluate(String a)
         {
             //(1+(2*3))
-            Stack<float> val = new Stack<float>();
-            Stack<char> op = new Stack<char>();
-
+            
+            
+            string tmp="";
             bool Isdec = false;
 
-            float number = 0,decimal_pointer=1;
+            double number = 0,decimal_pointer=1;
 
             int size = a.Length;
 
             for (int i = 0; i < size; i++)
             {
                 if (char.IsWhiteSpace(a[i])) continue; //checks for whitespaces
-
-                //if it encounters '('
+                //if it encounters "("
                 if (a[i] == '(')
                 {
-                    op.Push('('); // pushes it top of op stack
+                    op.Push("("); // pushes it top of op stack
                 }
-                else if (a[i] == ')') //if it encounters ')' 
+                else if (a[i] == ')') //if it encounters ")" 
                 {
-                   //evaluates whats in between the brackets if the top of op stack is '('
-                    while (op.Count > 0 && (op.Peek() !='('))
+                   //evaluates whats in between the brackets if the top of op stack is "("
+                    while (op.Count > 0 && (op.Peek() !="("))
                     {
-                        float r = val.Pop();
-                        float s = val.Pop();
-                        val.Push(arthematic(s, r, op.Pop()));
+                        check_op();
+                        
+                        if (val.Count >= 2)
+                        {
+                            double r = val.Pop();
+                            double s = val.Pop();
+                            val.Push(arthematic(s, r, op.Pop()));
+                        }
                     }
                     op.Pop();
                 }
                 else if (char.IsDigit(a[i]))//if encounters digit it pushes it to top
                 {
-                   
                     number = 0;
                     Isdec = false;
                     decimal_pointer = 1;
                     while (i < size && (char.IsDigit(a[i]) || a[i] == '.'))//if any decimal no.
                     {
-
                         if (a[i] == '.')//evlautaion after deciaml point
                         {
                             if(Isdec){
@@ -317,35 +438,58 @@ namespace WpfApp1
                         else{ //evlatuion before decimal point
                             number = number * 10 + (a[i] - '0');
                         }
-
                         i++;
                     }
-
                     i--;
                     val.Push(number);//pushes no. when done 
+                       
                 }
                 else //pushes encountered operator in op stack checking the priority 
                 {
-                    while (op.Count > 0 && Precedence1(op.Peek()) >= Precedence1(a[i]))
+                    if (char.IsAsciiLetter(a[i]))
                     {
-                        float r = val.Pop();
-                        float s = val.Pop();
-                        val.Push(arthematic(s, r, op.Pop()));
+                        if (a[i] == 'e')
+                        {
+                            val.Push(e);
+                            
+                        }
+                        tmp += a[i];
+                        if (a[i + 1] == '(')
+                        {
+                            op.Push(tmp);
+                            tmp = "";
+                        }
                     }
-                    op.Push(a[i]);
-
+                    else
+                    {
+                        while (op.Count > 0 && Precedence1(op.Peek()) >= Precedence1(a[i].ToString()))
+                        {
+                            check_op();
+                            if (val.Count >= 2)
+                            {
+                                
+                                double r = val.Pop();
+                                double s = val.Pop();
+                                val.Push(arthematic(s, r, op.Pop()));
+                            }
+                        }
+                        op.Push(a[i].ToString());
+                    }
                 }
-                
-
             }
             //evalutes remaining operators outside the bracket
+            //if (op.Count == 0) throw new InvalidOperationException("invaild inputs");
             while (op.Count >0)
             {
-                float r = val.Pop();
-                float s = val.Pop();
-                val.Push(arthematic(s, r, op.Pop()));
+                check_op();
+                if (val.Count >=2)
+                {
+                    
+                    double r = val.Pop();
+                    double s = val.Pop();
+                    val.Push(arthematic(s, r, op.Pop()));
+                }
             }
-
             return val.Pop(); //only answers remains in the val stack
         }
 
